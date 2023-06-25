1673d735f0930d8982d7531106906be02b4d6373
Vincent Herlemont <vincent@herlemont.fr>
2023-06-25 23:30:18 +0200

    feat: add is_subtree

--- src/rooted_tree.rs
+++ src/rooted_tree.rs
@@ -84,53 +84,20 @@ impl<I: Eq + PartialEq + Clone + Hash, N: Node<I>> RootedTree<I, N> {
             0
         }
     }
+
+    pub fn is_subtree(&self) -> bool {
+        if let Some(root_node) = &self.root_node {
+            root_node.parent_id().is_some()
+        } else {
+            false
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    struct DataNode {
-        id: i32,
-        parent_id: Option<i32>,
-        child_ids: Vec<i32>,
-    }
-
-    impl DataNode {
-        pub fn new(id: i32) -> Self {
-            Self {
-                id,
-                parent_id: None,
-                child_ids: vec![],
-            }
-        }
-    }
-
-    impl Node<i32> for DataNode {
-        fn id(&self) -> i32 {
-            self.id
-        }
-
-        fn parent_id(&self) -> Option<i32> {
-            self.parent_id
-        }
-
-        fn child_ids_vec(&self) -> Vec<i32> {
-            self.child_ids.clone()
-        }
-
-        fn set_parent_id(&mut self, parent: i32) {
-            self.parent_id = Some(parent);
-        }
-
-        fn add_child_id(&mut self, child_id: i32) {
-            self.child_ids.push(child_id);
-        }
-
-        fn remove_child_id(&mut self, child_id: &i32) {
-            self.child_ids.retain(|id| id != child_id);
-        }
-    }
+    use crate::test_data::*;
 
     #[test]
     fn add_root_node() {
@@ -210,5 +177,18 @@ mod tests {
         assert_eq!(node.id(), 1);
         assert_eq!(node.parent_id(), None);
         assert_eq!(node.child_ids_vec(), vec![]);
+    }
+
+    #[test]
+    fn is_subtree() {
+        let mut r_tree = RootedTree::<i32, DataNode>::new();
+        let mut node = DataNode::new(1);
+        r_tree.add_node(None, node).unwrap();
+        assert_eq!(r_tree.is_subtree(), false);
+        let mut r_tree = RootedTree::<i32, DataNode>::new();
+        let mut node = DataNode::new(1);
+        node.set_parent_id(2);
+        r_tree.add_node(None, node).unwrap();
+        assert_eq!(r_tree.is_subtree(), true);
     }
 }
